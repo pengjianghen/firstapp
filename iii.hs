3e286210a524b19befbfcb60a28aea8d5a781741
Max Daniels <138215132+pengjianghen@users.noreply.github.com>
2023-11-02 10:13:33 -0400

    Create iii.hs

---
+++ iii.hs
@@ -0,0 +1,6 @@
+.sui-wraper .sui-dialog.sui-dialog-homepage-tts-pannel .homepage-tts-pannel-text{color:#fff;position:absolute;left:11px;top:8px}
+#bottom_layer{visibility:hidden;width:3000px;position:fixed;z-index:302;bottom:0;left:0;height:39px;padding-top:1px;zoom:1;margin:0;line-height:39px;background:#fff}
+#bottom_layer .lh{display:inline-block;margin-right:14px}
+#bottom_layer .lh .emphasize{text-decoration:underline;font-weight:700}
+#bottom_layer .lh:last-child{margin-left:-2px;margin-right:0}
+#bottom_layer .lh.activity{font-weight:700;text-decoration:underline}
